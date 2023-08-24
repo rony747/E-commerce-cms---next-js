eddea2eddad58f09f90c8f4338d208f9004f3d86
touhidul islam rony <touhid_rony@yahoo.com>
2023-08-25 03:39:48 +0600

    auth and modal

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import {Modal} from "@/components/ui/modal";
+import {useStoreModal} from "@/hooks/use-store-modal";
+import {useEffect} from "react";
+
+export default function SetupPage() {
+    const onOpen = useStoreModal((state)=>state.onOpen)
+    const isOpen = useStoreModal((state)=>state.isOpen)
+    useEffect(()=>{
+        if(!isOpen){
+            onOpen()
+        }
+    },[isOpen, onOpen])
+    return (
+
+       <div>
+         Root Page
+       </div>
+    )
+}
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {Modal} from "@/components/ui/modal";
+import {useStoreModal} from "@/hooks/use-store-modal";
+
+export const StoreModal = () => {
+    const storeModal = useStoreModal()
+    return (
+        <Modal title={"Create store"} description={'Add a New Store to manage products and categories'} isOpen={storeModal.isOpen}
+               onClose={storeModal.onClose}>
+            Future Create Store Form
+        </Modal>
+    )
+
+}
